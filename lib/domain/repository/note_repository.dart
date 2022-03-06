713b1bfe4012b48461e115f53442b0d027dede86
jason967 <jason967@naver.com>
2022-03-06 12:40:00 +0900

    fix etc

--- lib/domain/repository/note_repository.dart
+++ lib/domain/repository/note_repository.dart
@@ -2,7 +2,7 @@ import '../model/note.dart';
 
 abstract class NoteRepository {
   //노트 전체를 가져오는 메소드
-  Future<List<Note>> getNote();
+  Future<List<Note>> getNotes();
   //id를 이용해 노트를 가져오는 메소드
   Future<Note?> getNoteById(int id);
   //노트 생성
